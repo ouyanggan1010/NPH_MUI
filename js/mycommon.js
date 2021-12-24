ec146ce5094a58d1c76f7bbaef02b42fdd6dd5e8
ouyanggan1010 <1147846233@qq.com>
2021-12-24 19:10:59 +0800

    修改跳转事件

---
+++ js/mycommon.js
@@ -0,0 +1,19 @@
+// 返回的方法
+function backHtml() {
+    location.back();
+}
+function backMui() {
+    mui.back();
+}
+// 打开窗口的方法
+function openWindow(params) {
+    mui.openWindow({
+        ...params
+    });
+}
+// html替换当前页面
+function replaceHtml(url) {
+    location.replace(url)
+}
+// 计算在不同尺寸的屏幕下，下拉刷新的高度
+const refreshH = (($(window).width() * 60) / 390).toFixed(4);
