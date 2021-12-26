5b51c63b93639e8035bb21f46c3839ba606b7ae4
ouyanggan1010 <1147846233@qq.com>
2021-12-26 15:06:31 +0800

    修改下拉刷新上拉加载问题

--- js/mycommon.js
+++ js/mycommon.js
@@ -1,19 +1,19 @@
+// 计算在不同尺寸的屏幕下，下拉刷新的高度
+const refreshH = (($(window).width() * 55) / 390).toFixed(4);
 // 返回的方法
 function backHtml() {
-    location.back();
+  location.back();
 }
 function backMui() {
-    mui.back();
+  mui.back();
 }
 // 打开窗口的方法
 function openWindow(params) {
-    mui.openWindow({
-        ...params
-    });
+  mui.openWindow({
+    ...params,
+  });
 }
 // html替换当前页面
 function replaceHtml(url) {
-    location.replace(url)
+  location.replace(url);
 }
-// 计算在不同尺寸的屏幕下，下拉刷新的高度
-const refreshH = (($(window).width() * 60) / 390).toFixed(4);
